fc7ecf1a19381c77551b92f9c2d37c8811961e8f
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2016-01-06 12:10:49 -0500

    bower initial

--- dist/index.js
+++ dist/index.js
@@ -18,18 +18,20 @@ var _ramda = require('ramda');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+//TODO extract these into "shortcuts" module
+var l = console.log.bind(console);
+var err = console.error.bind(console);
+
 var argv = (0, _minimist2.default)(process.argv.slice(2), { alias: { 'a': 'httpauth' } });
 var app = (0, _express2.default)();
 
+// http auth setup
+// create httppasswd file & pass path to it relative to cwd (or absolute)
 if ((0, _ramda.not)((0, _ramda.has)('httpauth')(argv))) throw "ERROR: --httpauth, -a required";
-
-//http auth setup
 var basic = _httpAuth2.default.basic({
   realm: "presentations",
   file: (0, _path.resolve)(process.cwd(), argv.httpauth)
 });
-
-// Application setup.
 app.use(_httpAuth2.default.connect(basic));
 
 // Setup route.
@@ -37,4 +39,7 @@ app.get('/', function (req, res) {
   res.send("Hello from express - " + req.user + "!");
 });
 
-app.listen(8080);
+var port = process.env.PORT || 8080;
+app.listen(port, function (app) {
+  return l('listening on %s', port);
+});
--- src/index.js
+++ src/index.js
@@ -4,18 +4,20 @@ import express from 'express';
 import auth from 'http-auth';
 import {not, has} from 'ramda';
 
+//TODO extract these into "shortcuts" module
+const l = console.log.bind(console);
+const err = console.error.bind(console);
+
 const argv = minimist(process.argv.slice(2), {alias:{'a':'httpauth'}});
 const app = express();
 
+// http auth setup
+// create httppasswd file & pass path to it relative to cwd (or absolute)
 if(not(has('httpauth')(argv))) throw "ERROR: --httpauth, -a required";
-
-//http auth setup
 const basic = auth.basic({
   realm: "presentations",
   file: resolve(process.cwd(), argv.httpauth)
 });
-
-// Application setup. 
 app.use(auth.connect(basic));
  
 // Setup route. 
@@ -23,4 +25,5 @@ app.get('/', function(req, res){
   res.send("Hello from express - " + req.user + "!");
 });
 
-app.listen(8080);
+const port = process.env.PORT || 8080;
+app.listen(port, app => l('listening on %s', port));
